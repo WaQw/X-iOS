b0c9edcb96fa3ee335c452cfa12494d8ddea6986
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-15 14:24:19 -0400

    init

---
+++ X-iOS/X_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_iOSApp.swift
+//  X-iOS
+//
+//  Created by Anqi on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct X_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
