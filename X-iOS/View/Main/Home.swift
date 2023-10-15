06f90eb8a03d79a89849c3ea5e9a0684010ecec3
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-15 14:29:01 -0400

    Add assets

--- X-iOS/View/Main/Home.swift
+++ X-iOS/View/Main/Home.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                TabView {
+                    
+                }
+            }
+        }
     }
 }
 
