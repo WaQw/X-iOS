e322723bbfdec99a69b2920b08de056f448e22f0
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-15 22:50:31 -0400

    Add tweet cell view

---
+++ X-iOS/View/Feed/TweetCellView.swift
@@ -0,0 +1,92 @@
+//
+//  TweetCellView.swift
+//  X-iOS
+//
+//  Created by Anqi on 2023/10/15.
+//
+
+import SwiftUI
+
+struct TweetCellView: View {
+    
+    var tweet: String
+    var tweetImage: String?
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 10, content: {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 55, height: 55)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 10, content: {
+                    Text("John ")
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                    +
+                    Text("@john_salta")
+                        .foregroundColor(.gray)
+                    
+                    Text(tweet)
+                        .frame(maxHeight: 100, alignment: .top)
+                    
+                    if let image = tweetImage {
+                        GeometryReader { proxy in
+                            Image(image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: proxy.frame(in: .global).width, height: 250)
+                                .cornerRadius(15)
+                        }
+                    }
+                })
+            })
+                
+            // Cell Bottom
+            HStack(spacing: 50, content: {
+                Button(action: {
+                    
+                }, label: {
+                    Image("Comments")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                }).foregroundColor(.gray)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image("Retweet")
+                        .resizable()
+                        .frame(width: 18, height: 14)
+                }).foregroundColor(.gray)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image("love")
+                        .resizable()
+                        .frame(width: 18, height: 15)
+                }).foregroundColor(.gray)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image("upload")
+                        .resizable()
+                        .frame(width: 18, height: 15)
+                }).foregroundColor(.gray)
+            })
+            .padding(.top, 4)
+        }
+    }
+}
+
+struct TweetCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetCellView(tweet: sampleText)
+    }
+}
+
+var sampleText = "As you begin designing your app or game for iOS, start by understanding the following fundamental device characteristics and patterns that distinguish the iOS experience. Using these characteristics and patterns to inform your design decisions can help you provide an app or game that iPhone users appreciate."
