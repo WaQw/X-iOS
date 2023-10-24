119122ba9d437dd78e7630c412c38d71ff8fada1
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-24 14:29:58 -0400

    Create user model
    with validator

---
+++ X-API/src/models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
+  avatar: {
+    type: Buffer,
+  },
+  avatarExists: {
+    type: Boolean,
+  },
+  bio: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  followers: {
+    type: Array,
+    default: [],
+  },
+  followings: {
+    type: Array,
+    default: [],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
