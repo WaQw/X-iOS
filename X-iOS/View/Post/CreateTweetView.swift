e797027537d35fa9a50bdbf211735b30be3eb13c
WaQw <47028829+WaQw@users.noreply.github.com>
2023-11-01 18:38:53 -0400

    Add UI for uploading images

--- X-iOS/View/Post/CreateTweetView.swift
+++ X-iOS/View/Post/CreateTweetView.swift
@@ -12,6 +12,9 @@ struct CreateTweetView: View {
     @State var text = ""
     @Binding var show: Bool
     @ObservedObject var viewModel = CreateTweetViewModel()
+    @State var imagePickerPresented = false
+    @State var selectedImage: UIImage?
+    @State var postImage: Image?
     
     var body: some View {
         VStack {
@@ -38,7 +41,49 @@ struct CreateTweetView: View {
             }
             
             MultilineTextField(text: $text)
+            
+            if postImage == nil { // Before selection: add button
+                Button(action: {
+                    self.imagePickerPresented.toggle()
+                }, label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 70, height: 70)
+                        .clipped()
+                        .padding(.top)
+                        .foregroundColor(.black)
+                })
+                .sheet(isPresented: $imagePickerPresented) {
+                    loadImage()
+                } content: {
+                    ImagePicker(image: $selectedImage)
+                }
+            } else if let image = postImage { // After selection: image preview
+                VStack {
+                    HStack(alignment: .top) {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: getRect().width * 0.9)
+                            .padding(.horizontal)
+                            .cornerRadius(16)
+                            .clipped()
+                    }
+                    .padding()
+                    
+                    Spacer()
+                }
+            }
         }
         .padding()
+    }
+}
+
+extension CreateTweetView {
+    // Turn UIImage into SwiftUI Image
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        postImage = Image(uiImage: selectedImage)
     }
 }
